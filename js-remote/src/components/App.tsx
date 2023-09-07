c91a71d6bb170c1aa4a3ed8aa2f9348a1d18b968
lewiswolf <lewiswolstanholme@gmail.com>
2023-09-07 11:09:38 +0000

    implemented fuzzy logic for gui and pepper

--- js-remote/src/components/App.tsx
+++ js-remote/src/components/App.tsx
@@ -24,6 +24,8 @@ export default function App(): JSX.Element {
 			window.removeEventListener('Beladisconnected', belaOff)
 		}
 	}, [])
+	// handle bela callback
+	const [polygonUpdated, setPolygonUpdated] = useState<0 | 1>(0)
 
 	return (
 		<>
@@ -31,17 +33,19 @@ export default function App(): JSX.Element {
 				<Drum
 					N={10} // will add controls for this later
 					// onPolygonChange={(P: Polygon) => console.log(`Polygon changed: ${P}`)}
-					onPolygonChange={(P: Polygon) =>
+					onPolygonChange={(P: Polygon) => {
+						Bela.sendBuffer(0, 'float', polygonUpdated ? 0 : 1)
+						setPolygonUpdated(polygonUpdated ? 0 : 1)
 						Bela.sendBuffer(
-							0,
+							1,
 							'float',
 							interpolateLineRange(P, 64)
 								.map((p: Point) => [p.x, p.y])
 								.flat(),
 						)
-					}
+					}}
 					// onStrikeChange={(p: Point) => console.log(`Polygon changed: ${p}`)}
-					onStrikeChange={(p: Point) => Bela.sendBuffer(1, 'float', [p.x, p.y])}
+					onStrikeChange={(p: Point) => Bela.sendBuffer(2, 'float', [p.x, p.y])}
 				/>
 			) : (
 				<p>There is no Bela connected... 🧑‍💻</p>
