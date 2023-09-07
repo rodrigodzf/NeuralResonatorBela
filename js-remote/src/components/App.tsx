8604baadb40676ebae57bb640084a50437c33f15
lewiswolf <lewiswolstanholme@gmail.com>
2023-09-07 11:09:38 +0000

    added rodrigos interpolation method

--- js-remote/src/components/App.tsx
+++ js-remote/src/components/App.tsx
@@ -6,6 +6,7 @@ import '../scss/App.scss'
 import Bela from '../BelaAPI'
 import { Point, Polygon } from '../geometry'
 import { Drum } from './drum'
+import { interpolateLineRange } from './polygon2buffer'
 
 export default function App(): JSX.Element {
 	// is the Bela ws connected?
@@ -26,13 +27,19 @@ export default function App(): JSX.Element {
 
 	return (
 		<>
+			{/* {true ? ( */}
 			{belaLoaded ? (
 				<Drum
 					N={10} // will add controls for this later
 					// onPolygonChange={(P: Polygon) => console.log(`Polygon changed: ${P}`)}
-					onPolygonChange={(P: Polygon) =>
+					onPolygonChange={(P: Polygon) => {
+						console.log(P)
+						console.log(P.length)
+					    P = interpolateLineRange(P, 64)
+						console.log(P)
+						console.log(P.length)
 						Bela.sendBuffer(0, 'float', P.map((p: Point) => [p.x, p.y]).flat())
-					}
+					}}
 					// onStrikeChange={(p: Point) => console.log(`Polygon changed: ${p}`)}
 					onStrikeChange={(p: Point) => Bela.sendBuffer(1, 'float', [p.x, p.y])}
 				/>
